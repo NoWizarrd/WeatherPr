fc1f29e0719e5261c3d492919364fb7312090158
Aleksey Pnev <pnev.leha@mail.ru>
2023-12-11 23:55:23 +0700

    Map changes

--- src/modules/api/YandexMap.tsx
+++ src/modules/api/YandexMap.tsx
@@ -1,14 +1,15 @@
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark, GeolocationControl, Rectangle } from '@pbe/react-yandex-maps';
+import { YMapGeolocationControl } from '@yandex/ymaps3-types/packages/controls';
 
-const mapState = { center: [55.76, 37.64], zoom: 10 };
+const mapState = { center: [56.02, 92.92], zoom: 10 };
 
 export const YandexMap = () => (
   <YMaps>
-    <Map state={mapState}>
+    <Map state={mapState} width='80vw' height='90vh'>
 
       <Placemark
         geometry={{
-          coordinates: [55.751574, 37.573856]
+          coordinates: [56.0043, 92.5217]
         }}
         properties={{
           hintContent: 'Собственный значок метки',
@@ -21,7 +22,8 @@ export const YandexMap = () => (
           iconImageOffset: [-3, -42]
         }}
       />
-
+      <GeolocationControl />
+      <Placemark geometry={[56.02, 92.92]}/>
     </Map>
   </YMaps>
 );
